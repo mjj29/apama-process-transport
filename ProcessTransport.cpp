857d4685c352638a2b33657620feeafaeaa677f9
Matthew Johnson <matt@matthew.ath.cx>
2018-10-26 22:16:30 +0100

    update to work on windows as well

--- ProcessTransport.cpp
+++ ProcessTransport.cpp
@@ -1,9 +1,22 @@
+#include<cinttypes>
 #include<sag_connectivity_plugins.hpp>
 #include<string>
 #include<thread>
-#include<unistd.h>
 #include<stdio.h>
-#include <fcntl.h>
+#include<fcntl.h>
+
+#ifdef _WIN32
+
+#include <windows.h> 
+#include <tchar.h>
+#include <strsafe.h>
+#define BUFSIZE 4096
+
+#else
+
+#include <unistd.h>
+
+#endif
 
 using com::softwareag::connectivity::AbstractSimpleTransport;
 using com::softwareag::connectivity::MapExtractor;
@@ -13,6 +26,8 @@ using com::softwareag::connectivity::map_t;
 using com::softwareag::connectivity::data_t;
 using com::softwareag::connectivity::get;
 using com::softwareag::connectivity::convert_to;
+
+using namespace com::softwareag::connectivity;
 
 namespace com { namespace apamax {
 
@@ -44,6 +59,75 @@ public:
 private:
 	static void runProcess(ProcessTransport *transport)
 	{
+#ifdef _WIN32
+		HANDLE g_hChildStd_OUT_Rd = NULL;
+		HANDLE g_hChildStd_OUT_Wr = NULL;
+		SECURITY_ATTRIBUTES saAttr; 
+		saAttr.nLength = sizeof(SECURITY_ATTRIBUTES); 
+		saAttr.bInheritHandle = TRUE; 
+		saAttr.lpSecurityDescriptor = NULL; 
+
+		// Create a pipe for the child process's STDOUT. 
+		if ( ! CreatePipe(&g_hChildStd_OUT_Rd, &g_hChildStd_OUT_Wr, &saAttr, 0) ) {
+			transport->logger.error("Failed to create subprocess pipe"); 
+			return;
+		}
+
+		// Ensure the read handle to the pipe for STDOUT is not inherited.
+		if ( ! SetHandleInformation(g_hChildStd_OUT_Rd, HANDLE_FLAG_INHERIT, 0) ) {
+			transport->logger.error("Failed to create subprocess pipe"); 
+			return;
+		}
+
+		std::ostringstream oss;
+		for (auto it = transport->command.begin(); it != transport->command.end(); ++it) {
+			oss << '"' << get<const char*>(*it) << "\" ";
+		}
+
+		PROCESS_INFORMATION piProcInfo; 
+		STARTUPINFO siStartInfo;
+		BOOL bSuccess = false; 
+ 
+		ZeroMemory( &piProcInfo, sizeof(PROCESS_INFORMATION) );
+		ZeroMemory( &siStartInfo, sizeof(STARTUPINFO) );
+		siStartInfo.cb = sizeof(STARTUPINFO); 
+		siStartInfo.hStdError = g_hChildStd_OUT_Wr;
+		siStartInfo.hStdOutput = g_hChildStd_OUT_Wr;
+		siStartInfo.dwFlags |= STARTF_USESTDHANDLES;
+ 
+// Create the child process. 
+    
+		bSuccess = CreateProcessA(get<const char*>(transport->command[0]), 
+				const_cast<char*>(oss.str().c_str()),     // command line 
+				nullptr,       // process security attributes 
+				nullptr,       // primary thread security attributes 
+				true,          // handles are inherited 
+				0,             // creation flags 
+				nullptr,       // use parent's environment 
+				nullptr,       // use parent's current directory 
+				&siStartInfo,  // STARTUPINFO pointer 
+				&piProcInfo);  // receives PROCESS_INFORMATION 
+
+		// If an error occurs, exit the application. 
+		if ( ! bSuccess ) {
+			transport->logger.error("Failed to create sub-process");
+			return;
+		} else {
+			CloseHandle(piProcInfo.hProcess);
+			CloseHandle(piProcInfo.hThread);
+		}
+
+		CHAR chBuf[BUFSIZE]; 
+		char *r;
+		HANDLE hParentStdOut = GetStdHandle(STD_OUTPUT_HANDLE);
+		FILE *fp = fdopen(_open_osfhandle((intptr_t)g_hChildStd_OUT_Rd, _O_RDONLY), "rb");
+
+		while ((r = fgets(chBuf, BUFSIZE, fp)) || errno==EAGAIN )
+		{ 
+			Message m{data_t{chBuf}};
+			transport->hostSide->sendBatchTowardsHost(&m, &m+1);
+		} 
+#else
 		int pipeForStdOut[2];
 		pipe(pipeForStdOut);
 		transport->childPid = fork();
@@ -79,6 +163,7 @@ private:
 			close(pipeForStdOut[0]);
 			transport->logger.info("Transport finished reading");
 		}
+#endif
 	}
 	void killProcess()
 	{
